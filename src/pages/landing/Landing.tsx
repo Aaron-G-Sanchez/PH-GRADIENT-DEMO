9c1eed4d8a24b8cdeae38c0d441816f3656d1e3d
Aaron Sanchez <saarong12@gmail.com>
2024-05-14 00:33:16 -0500

    Added some content placeholders and adjusted the background image

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -14,15 +14,38 @@ export const Landing = () => {
           </div>
         </div>
         <div className='info-container'>
-          <div>
-            <p>Info box #1</p>
+          <div className='display-container'>
+          </div>
+          <div className='dialog-container'>
+            <h2>Go beyond just games.</h2>
+            <p>Get matched with a liked minded individual to help rank up, beat the boss, or make a new friend.</p>
           </div>
         </div>
         <div className='info-container'>
-          <div>
-            <p>Info box #2</p>
+           <div className='dialog-container'>
+            <h2>Communicate with ease.</h2>
+            <p>Once matched, communicate via our low-latency text chat.</p>
+          </div>
+          <div className='display-container'>
           </div>
         </div>
+        <div className='info-container'>
+          <div className='display-container'>
+          </div>
+           <div className='dialog-container'>
+            <h2>Not the vibes?</h2>
+            <p>No worries! Rematch with ease or leave the party to find a new partner.</p>
+          </div>
+        </div>
+
+        <footer className='footer-container'>
+          <ul className='links-list'>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+        </footer>
 
       </main>
     </>
